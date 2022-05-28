fe7249eb5fdf0f20bb3911e8ea7bb41fe34ef23e
Adam Patterson <adam@adamrt.com>
2022-05-28 14:32:06 +0700

    Remove multiselect functionality and remaining editing functionality

--- ganesha/ui.py
+++ ganesha/ui.py
@@ -226,135 +226,33 @@ class ViewerState(FSM):
 class MapViewer(DirectObject):
     def __init__(self, *args, **kwargs):
         self.base = ShowBase()
+
         wp = WindowProperties()
         wp.setTitle("Ganesha")
         self.base.win.requestProperties(wp)
+
         self.state = ViewerState(self, "viewer_state")
         self.mouse = ViewerMouse(self)
+
         self.world = World(self)
+
         self.selected_object = None
         self.full_light_enabled = False
         self.terrain_mode = MESH_ONLY
         self.width = None
         self.height = None
+
+        # Keybindings
         self.accept("window-event", self.on_window_event)
         self.accept("]", self.next_situation)
         self.accept("[", self.prev_situation)
         self.accept("n", self.next_gns)
         self.accept("t", self.next_terrain_mode)
+        self.accept("escape", self.open_settings_window)
 
         self.base.disableMouse()
         self.state.request("Spin")
         self.selected_objects = []
-        self.multiSelect = False
-        self.accept("control", self.multi_select)
-        self.accept("control-up", self.end_multi_select)
-        self.accept("q", self.decrease_Y)
-        self.accept("e", self.increase_Y)
-        self.accept("arrow_down", self.decrease_X)
-        self.accept("arrow_up", self.increase_X)
-        self.accept("arrow_right", self.decrease_Z)
-        self.accept("arrow_left", self.increase_Z)
-        self.accept("escape", self.open_settings_window)
-        self.accept("control-a", self.select_all)
-        self.select_all_mode = 0
-        # self.base.messenger.toggleVerbose()
-
-    # TODO: move these functions somewhere after start (organize)
-
-    def multi_select(self):
-        self.multiSelect = True
-
-    def end_multi_select(self):
-        self.multiSelect = False
-
-    def select_all(self):
-        # Needs special version of unselect to avoid crashes
-        for obj in self.selected_objects:
-            obj.unselect()
-            self.selected_objects = []
-        if self.selected_object:
-            self.selected_object.unselect()
-            self.selected_object = None
-
-        if self.terrain_mode == MOSTLY_TERRAIN or self.terrain_mode == TERRAIN_ONLY:
-            if self.select_all_mode == 2:
-                self.select_all_mode = 0
-            else:
-                self.select_all_mode += 1
-            if self.select_all_mode == 2:
-                for i in range(len(self.world.terrain.tiles)):
-                    for j in range(len(self.world.terrain.tiles[i])):
-                        for p in range(len(self.world.terrain.tiles[i][j])):
-                            self.world.terrain.tiles[i][j][p].select()
-                            self.selected_objects.append(
-                                self.world.terrain.tiles[i][j][p]
-                            )
-            else:
-                for j in range(len(self.world.terrain.tiles[self.select_all_mode])):
-                    for p in range(
-                        len(self.world.terrain.tiles[self.select_all_mode][j])
-                    ):
-                        self.world.terrain.tiles[self.select_all_mode][j][p].select()
-                        self.selected_objects.append(
-                            self.world.terrain.tiles[self.select_all_mode][j][p]
-                        )
-        else:
-            for obj in self.world.polygons:
-                obj.select()
-                self.selected_objects.append(obj)
-
-    def increase_Y(self):
-        if len(self.selected_objects) > 0:
-            if isinstance(self.selected_objects[0], Polygon):
-                self.world.move_selected_poly("Y", 12, 1, self.selected_objects)
-            else:  # It is a tile
-                self.world.move_selected_tile(1, self.selected_objects)
-        elif isinstance(self.selected_object, Polygon):
-            self.world.move_selected_poly("Y", 12, 1, [self.selected_object])
-        else:  # It is a tile
-            if self.selected_object:
-                self.world.move_selected_tile(1, [self.selected_object])
-
-    def decrease_Y(self):
-        if len(self.selected_objects) > 0:
-            if isinstance(self.selected_objects[0], Polygon):
-                self.world.move_selected_poly("Y", 12, -1, self.selected_objects)
-            else:  # It is a tile
-                self.world.move_selected_tile(-1, self.selected_objects)
-        elif isinstance(self.selected_object, Polygon):
-            self.world.move_selected_poly("Y", 12, -1, [self.selected_object])
-        else:  # It is a tile
-            if self.selected_object:
-                self.world.move_selected_tile(-1, [self.selected_object])
-
-    def increase_X(self):
-        if len(self.selected_objects) > 0:
-            if isinstance(self.selected_objects[0], Polygon):
-                self.world.move_selected_poly("X", 28, 1, self.selected_objects)
-        elif isinstance(self.selected_object, Polygon):
-            self.world.move_selected_poly("X", 28, 1, [self.selected_object])
-
-    def decrease_X(self):
-        if len(self.selected_objects) > 0:
-            if isinstance(self.selected_objects[0], Polygon):
-                self.world.move_selected_poly("X", 28, -1, self.selected_objects)
-        elif isinstance(self.selected_object, Polygon):
-            self.world.move_selected_poly("X", 28, -1, [self.selected_object])
-
-    def increase_Z(self):
-        if len(self.selected_objects) > 0:
-            if isinstance(self.selected_objects[0], Polygon):
-                self.world.move_selected_poly("Z", 28, 1, self.selected_objects)
-        elif isinstance(self.selected_object, Polygon):
-            self.world.move_selected_poly("Z", 28, 1, [self.selected_object])
-
-    def decrease_Z(self):
-        if len(self.selected_objects) > 0:
-            if isinstance(self.selected_objects[0], Polygon):
-                self.world.move_selected_poly("Z", 28, -1, self.selected_objects)
-        elif isinstance(self.selected_object, Polygon):
-            self.world.move_selected_poly("Z", 28, -1, [self.selected_object])
 
     def start(self, gns_path):
         self.wx_app = wx.App(0)
@@ -441,31 +339,10 @@ class MapViewer(DirectObject):
     def select(self, hovered_object):
         self.unselect()
         if hovered_object:
-            if self.multiSelect:
-                # If the object is not already selected
-                if hovered_object not in self.selected_objects:
-                    # If the list is empty, or if the types in the list match the type selected (so we don't mix Tile / Poly)
-                    if len(self.selected_objects) == 0 or type(
-                        self.selected_objects[0]
-                    ) == type(hovered_object):
-                        self.selected_objects.append(hovered_object)
-                        hovered_object.select()
-                    # If the user tries to multi-select polygons and tiles together, don't
-                    else:
-                        pass
-                # If the object is already selected
-                else:
-                    self.selected_objects.remove(hovered_object)
-                    hovered_object.unselect()
-            else:
-                self.selected_object = hovered_object
-                self.selected_object.select()
+            self.selected_object = hovered_object
+            self.selected_object.select()
 
     def unselect(self):
-        if not self.multiSelect:
-            for obj in self.selected_objects:
-                obj.unselect()
-            self.selected_objects = []
         if self.selected_object:
             self.selected_object.unselect()
             self.selected_object = None
@@ -507,14 +384,7 @@ class SettingsWindow(wx.Frame):
         text = (
             "[: Previous state\t]: Next State\n\n"
             + "n: Next map\t\tt: Next terrain mode\n\n"
-            + "s: Save\t\t\to: Output texture\n\n"
             + "Alt-Right Click / Mouse-Wheel Click + Drag: Pan Camera\n\n"
-            + "CTRL + A: Select all Polygons / Tiles depending on view.\nCTRL + A repeatedly: In terrain view, cycle through multiple levels.\n\n"
-            + "Holding CTRL: Allows selection of multiple polygons\non which you can perform the following on:\n\n"
-            + "q: Decrease Y (12)\te: Increase Y (12)\n\n"
-            + "q: Decrease Tile height (1) \te: Increase Tile height (1)\n\n"
-            + "Up: Increase X (28)\tDown: Decrease X (28)\n\n"
-            + "Left: Increase Z (28)\tRight: Decrease Z (28)\n\n"
         )
         text_label = wx.StaticText(panel, wx.ID_ANY, text)
         textArea = wx.BoxSizer(wx.HORIZONTAL)
